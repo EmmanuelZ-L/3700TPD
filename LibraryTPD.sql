e5cfb2cc7199823dfadc8145f20b9a382449fa3f
EmmanuelZ-L <42583606+EmmanuelZ-L@users.noreply.github.com>
2018-12-12 02:08:14 -0500

    Update LibraryTPD.sql
    Added data to database. Allows user to add to their tables for reading status.

--- LibraryTPD.sql
+++ LibraryTPD.sql
@@ -9,7 +9,7 @@ CREATE TABLE theUser (
 
 
 CREATE TABLE book (
-	ISBN character(13) PRIMARY KEY,
+	ISBN character(20) PRIMARY KEY,
 	Name character (150),
 	Genre character(20)
 );
@@ -54,6 +54,18 @@ CREATE TABLE willRead (
 	usID character (10) REFERENCES theUser,
 	PRIMARY KEY (bookISBN, usID)
 );
+INSERT INTO theUser(userID, fname, lname, Address, Gender, Birthday)
+VALUES ('0000000000', 'John', 'Doe', '1738 Squad Road', 'M', '1993-03-11'); 
+
+INSERT INTO Book (ISBN, Name, Genre, Author)
+VALUES ('0-8-7113-993-6', 'The Last Stand of Fox Company', 'Non-Fiction', 'Bob Drury'),
+('0-8173-1161-0', 'China Marine', 'Non-Fiction', 'E.B. Sledge'),
+('0-87011-355- 0', 'The Reluctant Admiral: Yamamoto and the Imperial Navy', 'Non-Fiction', 'Hiroyuki Agawa'),
+('9781338299144', 'Harry Potter and the Philosophers Stone', 'Fiction', 'J.K. Rowling'),
+('9780439064873', 'Harry Potter and the Chamber of Secrets', 'Fiction', 'J.K. Rowling'),
+('9780747542155', 'Harry Potter and the Prisoner of Azkaban', 'Fiction', 'J.K. Rowling'),
+('9780345342966', 'Fahrenheit 451', 'Fiction', 'Ray Bradbury');
+
 
 
 
